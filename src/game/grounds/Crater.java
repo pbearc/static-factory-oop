3d94c0d8ff290c8a51496ddd223777ebd5be660a
behhanyu <hbeh0003@student.monash.edu>
2024-04-04 17:57:24 +0800

    Add Crater which extends Ground, make it spawns HuntsmanSpider

---
+++ src/game/grounds/Crater.java
@@ -0,0 +1,21 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actors.HuntsmanSpider;
+
+public class Crater extends Ground {
+
+    public Crater() {
+        super('u');
+    }
+
+    @Override
+    public void tick(Location location) {
+        if(Math.random() <= 0.05 && !location.containsAnActor()){
+            location.addActor(new HuntsmanSpider());
+        }
+    }
+
+
+}
