cc0e2dcddaeb0e508afae70d4a6552cac17bcb57
behhanyu <hbeh0003@student.monash.edu>
2024-04-04 17:51:20 +0800

    Add Consume Action that extends Action Show consume in menu when ConsumeAction is triggered

---
+++ src/game/actions/ConsumeAction.java
@@ -0,0 +1,32 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.Player;
+import game.items.Consumable;
+
+public class ConsumeAction extends Action {
+
+    Consumable item;
+
+    public ConsumeAction(Consumable item){
+        this.item = item;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        Player player = (Player) actor;
+        boolean retVal = item.consumed(player,map);
+        if (!retVal){
+            return "The" + item + "is empty.";
+        }
+        return menuDescription(player);
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        Player player = (Player) actor;
+        return player + " consumes " + item;
+    }
+}
