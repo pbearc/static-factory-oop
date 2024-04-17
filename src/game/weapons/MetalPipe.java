6c4d8c84b58e5a5ae17d0f6b34630f83bdcf0437
behhanyu <hbeh0003@student.monash.edu>
2024-04-17 15:19:41 +0800

    Delete PickUpWeaponAction implements it logic in MetalPipe allowableAction

--- src/game/weapons/MetalPipe.java
+++ src/game/weapons/MetalPipe.java
@@ -2,19 +2,15 @@ package game.weapons;
 
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.items.PickUpAction;
 import edu.monash.fit2099.engine.positions.Location;
 import edu.monash.fit2099.engine.weapons.WeaponItem;
 import game.actions.AttackAction;
-import game.actions.ConsumeAction;
-import game.actions.PickUpWeaponAction;
 import game.capabilities.Status;
 
 public class MetalPipe extends WeaponItem {
 
     public MetalPipe() {
         super("Metal Pipe", '!', 1, "hits", 20);
-//        this.addCapability(Status.HIT_WITH_PIPE);
     }
 
     @Override
