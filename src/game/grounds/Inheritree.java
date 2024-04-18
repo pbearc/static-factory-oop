e96d3a5fc2769e9b16886b603ceaa71ba4b23a17
behhanyu <hbeh0003@student.monash.edu>
2024-04-18 18:30:54 +0800

    Remove produceFruit logic from Inheritree, make it inside SaplingTree and MatureTree

--- src/game/grounds/Inheritree.java
+++ src/game/grounds/Inheritree.java
@@ -10,50 +10,56 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Inheritree extends SpawningGround {
+public abstract class Inheritree extends SpawningGround {
 
     private int age = 0;
-    private static final char SAPLING_CHAR = 't';
-    private static final char MATURE_CHAR = 'T';
+//    private static final char SAPLING_CHAR = 't';
+//    private static final char MATURE_CHAR = 'T';
 
 
-    public Inheritree() {
-        super(SAPLING_CHAR);
+    public Inheritree(char displayChar) {
+        super(displayChar);
 //        addCapability(Status.ALIVE);
     }
 
     @Override
     public void tick(Location location) {
 //        super.tick(location);
+
         age++;
-        if (age > 5 && this.getDisplayChar() == SAPLING_CHAR){
-            setDisplayChar(MATURE_CHAR);
-        }
-
-        // Produce fruits
-        if (this.getDisplayChar() == SAPLING_CHAR) {
-            produceFruit(location, 0.3);
-        } else if (this.getDisplayChar() == MATURE_CHAR) {
-            produceFruit(location, 0.2);
-        }
+        produceFruit2(location, age);
+//        if (age > 5 && this.getDisplayChar() == SAPLING_CHAR){
+//            setDisplayChar(MATURE_CHAR);
+//        }
+//
+//        // Produce fruits
+//        if (this.getDisplayChar() == SAPLING_CHAR) {
+//            produceFruit(location, 0.3);
+//        } else if (this.getDisplayChar() == MATURE_CHAR) {
+//            produceFruit(location, 0.2);
+//        }
     }
 
-    private void produceFruit(Location location, double probability) {
-        if (Math.random() <= probability) {
-            List<Exit> exits = new ArrayList<>(location.getExits());
-            Collections.shuffle(exits); // Shuffle the list of exits
+    public void produceFruit2(Location location, int age) {
 
-            for (Exit exit : exits) {
-                if (!exit.getDestination().containsAnActor()) {
-                    if (this.getDisplayChar() == SAPLING_CHAR) {
-                        exit.getDestination().addItem(new SmallFruit());
-                    } else if (this.getDisplayChar() == MATURE_CHAR) {
-                        exit.getDestination().addItem(new BigFruit());
-                    }
-                    break; // Stop after adding fruit to one destination
-                }
-            }
-        }
     }
+
+//    private void produceFruit(Location location, double probability) {
+//        if (Math.random() <= probability) {
+//            List<Exit> exits = new ArrayList<>(location.getExits());
+//            Collections.shuffle(exits); // Shuffle the list of exits
+//
+//            for (Exit exit : exits) {
+//                if (!exit.getDestination().containsAnActor()) {
+//                    if (this.getDisplayChar() == SAPLING_CHAR) {
+//                        exit.getDestination().addItem(new SmallFruit());
+//                    } else if (this.getDisplayChar() == MATURE_CHAR) {
+//                        exit.getDestination().addItem(new BigFruit());
+//                    }
+//                    break; // Stop after adding fruit to one destination
+//                }
+//            }
+//        }
+//    }
 
 }
