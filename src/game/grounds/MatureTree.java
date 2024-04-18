8b781d419598ee88d365ade9b58ae20290936d05
behhanyu <hbeh0003@student.monash.edu>
2024-04-18 17:08:25 +0800

    Make SaplingTree and MatureTree class that extends Inheritree

---
+++ src/game/grounds/MatureTree.java
@@ -0,0 +1,33 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Location;
+import game.items.BigFruit;
+import game.items.Fruit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MatureTree extends Inheritree{
+    private BigFruit fruitToBeSpawn;
+    public MatureTree() {
+        super('T');
+        this.fruitToBeSpawn = new BigFruit();
+    }
+
+    public void produceFruit2(Location location, int age){
+        if (Math.random() <= 0.3) {
+            List<Exit> exits = new ArrayList<>(location.getExits());
+            Collections.shuffle(exits); // Shuffle the list of exits
+
+            for (Exit exit : exits) {
+                if (!exit.getDestination().containsAnActor()) {
+                    fruitToBeSpawn.spawn(exit.getDestination());
+                    break; // Stop after adding fruit to one destination
+                }
+            }
+        }
+
+    }
+}
---
+++ src/game/grounds/SaplingTree.java
@@ -0,0 +1,38 @@
+package game.grounds;
+
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Location;
+import game.items.BigFruit;
+import game.items.Fruit;
+import game.items.SmallFruit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SaplingTree extends Inheritree{
+    private SmallFruit fruitToBeSpawn;
+    public SaplingTree() {
+        super('t');
+        this.fruitToBeSpawn = new SmallFruit();
+    }
+
+    public void produceFruit2(Location location, int age){
+        if (age >= 5){
+            location.setGround(new MatureTree());
+        }
+        if (Math.random() <= 0.3) {
+            List<Exit> exits = new ArrayList<>(location.getExits());
+            Collections.shuffle(exits); // Shuffle the list of exits
+
+            for (Exit exit : exits) {
+                if (!exit.getDestination().containsAnActor()) {
+                    fruitToBeSpawn.spawn(exit.getDestination());
+                    break; // Stop after adding fruit to one destination
+                }
+            }
+        }
+
+    }
+
+}
