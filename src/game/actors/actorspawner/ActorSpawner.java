390389be8a0ab79af9c34374cca81fc76f62832a
behhanyu <hbeh0003@student.monash.edu>
2024-04-19 13:29:53 +0800

    Have ActorSpawner and ItemSpawner instead of Spawner only

---
+++ src/game/actors/actorspawner/ActorSpawner.java
@@ -0,0 +1,7 @@
+package game.actors.actorspawner;
+
+import edu.monash.fit2099.engine.positions.Location;
+
+public interface ActorSpawner {
+    void spawn(Location location);
+}
--- src/game/items/itemspawner/ItemSpawner.java
+++ src/game/items/itemspawner/ItemSpawner.java
@@ -1,7 +1,7 @@
-package game.spawners;
+package game.items.itemspawner;
 
 import edu.monash.fit2099.engine.positions.Location;
 
-public interface Spawner {
+public interface ItemSpawner {
     void spawn(Location location);
 }
